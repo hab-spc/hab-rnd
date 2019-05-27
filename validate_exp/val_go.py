92bc45fff983a231df7a5e88e653634183fe429a
Kevin Le <ktl014@ucsd.edu>
2019-05-27 14:16:59 -0700

    fix(val_go): loads correct csv file

--- validate_exp/val_go.py
+++ validate_exp/val_go.py
@@ -1,6 +1,7 @@
 import os
 import sys
-sys.path.insert(0, '/data6/lekevin/hab-spc/')
+print(os.path.abspath(os.path.join(__file__, '../..')))
+sys.path.insert(0, os.path.abspath(os.path.join(__file__, '../..')))
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,18 +12,18 @@ from validate_exp.v_utils import plot_results, compute_values, \
     load_density_data
 
 # Module level constants
-MICRO_COLUMNS = 'micro_proro,micro_total-phyto'.split(',')
+MICRO_COLUMNS = 'micro_proro'.split(',')
 PRORO_COLUMNS = ['micro_proro', 'corrected_Prorocentrum',
                  'clsfier_Prorocentrum']
 PHYTO_COLUMNS = 'spc_ImgCount,micro_total-phyto'.split(',')
 
 # Initialize csv filename
 data_dir = '/data6/lekevin/hab-master/hab-rnd/rawdata'
-csv_fname = os.path.join(data_dir, 'Density-{}_data.csv')
-df = load_density_data(csv_fname.format('all'), micro_col=MICRO_COLUMNS)
+csv_fname = os.path.join(data_dir, 'Density{}_data.csv')
+df = load_density_data(csv_fname.format('17-18-all'), micro_col=MICRO_COLUMNS)
 
-# time_dist = ['1min', '5min', '15min', 'H', '1H30min']
-time_dist = ['1min']
+time_dist = ['1min', '5min', '15min', '30min', '45min', '1h30min']
+# time_dist = ['1min']
 
 for idx, t in enumerate(time_dist):
     print('=' * 25 + f' Time Distribution: {t} ' + '=' * 25)
@@ -41,14 +42,17 @@ for idx, t in enumerate(time_dist):
 
     X, Y, Yerr, Xfit, Yfit = compute_values(columns, df, yerror=True,
                                             geometric_fit=False)
+
+    print('total time bins per sample: {}'.format(df[f'clsfier_proro_total_smpl_{t}'].mode()[0]))
     plot_results(X, Y, Yerr, Xfit, Yfit,
                  ax=ax, idx=0, n_rows=n_rows, n_cols=n_cols,
                  xlabel=xlabel, ylabel=ylabel, title=title)
 
     #     zoomed_df = df
-    zoomed_df = df[df.micro_proro < 4]
+    zoomed_df = df[df.micro_proro < 15]
     X, Y, Yerr, _, _ = compute_values(columns, zoomed_df, yerror=True)
     plot_results(X=X, Y=Y, Yerr=Yerr, Xfit=Xfit, Yfit=Yfit,
                  ax=ax, idx=1, n_rows=n_rows, n_cols=n_cols,
                  xlabel=xlabel, ylabel=ylabel, title=title)
+    plt.ylim(0,max(Y))
     plt.show()
