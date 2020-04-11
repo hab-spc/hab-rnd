18efe2c52077e67b4528a41d605ed0ca1ab02d77
Kevin Le <ktl014@ucsd.edu>
2020-04-10 17:37:57 -0700

    refactor(set_micro_counts): refactored micro csv formatting for clarity

--- prepare_db/set_micro_counts.py
+++ prepare_db/set_micro_counts.py
@@ -6,11 +6,22 @@ import pandas as pd
 
 from prepare_db.parse_csv import SPCParser
 
+CELLS_ML_FLAG = True
+
 csv_fname = '/data6/phytoplankton-db/csv/hab_micro_raw_2017_2019.csv'
+if CELLS_ML_FLAG:
+    csv_fname = csv_fname.replace('.csv', '_cells-mL.csv')
+
+# Load dataset
 NUM_CLASS = 9
 df = pd.read_csv(csv_fname)
 DATE_COL = 'SampleID'
 CELL_COUNT_LIMIT_COL = 'Cell Count Detection Limit'
+COUNTED_VOLUME_COL = 'Volume Counted (mL)'
+DATETIME_COL = 'datetime'
+CLASS_COL = 'class'
+CELLS_ML_COL = 'micro cells/mL'
+RAW_COUNT_COL = 'micro raw count'
 
 spc = SPCParser()
 
@@ -39,23 +50,40 @@ assert len(hab_species) == NUM_CLASS, f'Number of classes do not match {len(hab_
 # each cell ~ cells/Liter. Normalize this by the cell count detection limit to get the
 # raw counts
 temp = df[hab_species]
-# temp = temp.iloc[:,:].div(df[CELL_COUNT_LIMIT_COL], axis=0)
 
-# Stack into the counts csv
-temp = temp.stack().reset_index()
-temp = temp.rename({'level_0': 'datetime',
-                    'level_1': 'class',
-                    0: 'micro raw count'}, axis=1)
-temp['datetime'] = temp['datetime'].map(dates)
-temp['label'] = 'gtruth'
+
+def stack_count_data(data, metric_name):
+    data = data.stack().reset_index()
+    data = data.rename({'level_0': DATETIME_COL,
+                        'level_1': CLASS_COL,
+                        0: metric_name}, axis=1)
+    data[DATETIME_COL] = data[DATETIME_COL].map(dates)
+    return data
+
+
+# Get cells/mL
+converted_volume_rate = 1 / 1000
+temp_cells_ml = temp.iloc[:, :].multiply(1 / 1000, axis=0)
+temp_cells_ml = stack_count_data(temp_cells_ml, metric_name=CELLS_ML_COL)
+
+# Get raw count
+temp = temp.iloc[:, :].div(df[CELL_COUNT_LIMIT_COL], axis=0)
+temp = stack_count_data(temp, metric_name=RAW_COUNT_COL)
+
+# Merge raw counts and cells/mL into one dataframe
+temp = temp.merge(temp_cells_ml, on=[DATETIME_COL, CLASS_COL])
+
 
 # Get the relative abundance and total abundance
-temp['micro raw count'] = temp['micro raw count'].round()
-total_abundances = (temp.groupby('datetime')['micro raw count'].sum()).to_dict()
-temp['micro total abundance'] = temp['datetime'].map(total_abundances)
-temp['micro relative abundance'] = temp['micro raw count']/temp['micro total abundance']*100.0
-temp = temp.sort_values(by=['datetime', 'class'])
+temp['label'] = 'gtruth'
+temp[RAW_COUNT_COL] = temp[RAW_COUNT_COL].round()
+total_abundances = (temp.groupby(DATETIME_COL)[RAW_COUNT_COL].sum()).to_dict()
+temp['micro total abundance'] = temp[DATETIME_COL].map(total_abundances)
+temp['micro relative abundance'] = temp[RAW_COUNT_COL] / temp[
+    'micro total abundance'] * 100.0
+temp = temp.sort_values(by=[DATETIME_COL, CLASS_COL])
 
+# Save and backup dataset
 csv_fname = '/data6/phytoplankton-db/csv/hab_micro_2017_2019.csv'
 if os.path.exists(csv_fname):
     backedup_csv_fname = csv_fname + f'.{datetime.now().strftime("%Y%m%d")}'
@@ -63,9 +91,3 @@ if os.path.exists(csv_fname):
     shutil.copy(csv_fname, backedup_csv_fname)
 temp.to_csv(csv_fname, index=False)
 print(f'CSV saved as {csv_fname}')
-
-# # Merge with the pier dataset
-# temp = micro.merge(pier, on=['datetime', 'class'])
-# temp = temp.rename({'label_y': 'label'}, axis=1)
-# temp = temp.drop(['label_x'], axis=1)
-# temp = temp.sort_values(by=['datetime', 'label', 'class'], axis=1)
